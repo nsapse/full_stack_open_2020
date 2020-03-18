d5756b19c6ee36c66f57d817cc447f1e5009efa0
Noah Sapse <nsapse@gmail.com>
2020-03-18 17:42:46 -0400

    Implemented one second refresh counter

--- 1c/src/index.js
+++ 1c/src/index.js
@@ -23,4 +23,11 @@ const App = (props) => {
 
 let counter = 1
 
-ReactDOM.render(<App counter = {counter} />, document.getElementById("root"));
+const refresh = () => {
+  ReactDOM.render(<App counter = {counter} />, document.getElementById("root"));
+}
+
+setInterval(() => {
+  refresh()
+  counter +=1
+}, 1000);
