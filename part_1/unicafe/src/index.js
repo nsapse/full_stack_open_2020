1c395c2555118994f511c4327fbd070127535753
Noah Sapse <nsapse@gmail.com>
2020-03-19 18:05:20 -0400

    Completed exercise 1.6 (unicafe step 1) implementing feedback buttons and basic statistic display

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      code here
-    </div>
-  )
-}
+  // Defines a button which takes a return function and text as properties
+  const Button = ({ onClick, text }) => {
+    return <button onClick={onClick}>{text}</button>;
+  };
+  // Click-handler to increment good when clicked
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+  // Click-handler to increment neutral when clicked
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  // Click-handler to increment bad when clicked
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+  
+  return(
+      <div>
+          <h2>Give Feedback</h2>
+          <Button onClick = {handleBadClick} text = 'bad'></Button>
+          <Button onClick = {handleNeutralClick} text = 'neutral'></Button>
+          <Button onClick = {handleGoodClick} text = 'good'></Button>
+          <div>
+              <h2>
+                  Statistics
+              </h2>
+              <h1>Good: {good}</h1>
+              <h1>Neutral: {neutral}</h1>
+              <h1>Bad: {bad}</h1>
+          </div>
+      </div>
+
+  ) 
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
