0dc32bbd074ce52191d203dbcb5de8a4c07bb3b4
Noah Sapse <nsapse@gmail.com>
2020-03-19 10:46:54 -0400

    refactored so that state is passed to child components and buttons are examples of base class

--- 1c/src/index.js
+++ 1c/src/index.js
@@ -14,20 +14,47 @@ import ReactDOM from "react-dom";
 //   );
 // };
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0) 
+const App = props => {
+  const Display = ({counter}) => {
+    return <div>{counter}</div>;
+  };
+  const Button = (props) => {
+    return(
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    )
+  }
+  
+  const [counter, setCounter] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1); 
+  const setToZero = () =>  setCounter(0);
+  
+
   // const handleClick = () => {
   //   console.log("clicked")
   // }
-  
-  return(
-    <div>{counter}
-    <button onClick = {() => setCounter(counter+1)}>Plus</button>
-    <button onClick = {() => setCounter(0)}>Zero</button>
+
+  return (
+    <div>
+      <Display counter = {counter}/>
+      <Button
+        handleClick = {increaseByOne}
+        text = 'plus'
+      />
+      <Button
+        handleClick = {setToZero}
+        text = 'zero'
+      />
+      <Button
+        handleClick = {decreaseByOne}
+        text = 'minus'
+      />
     </div>
-  )
+  );
 };
 
-let counter = 1
+let counter = 1;
 
-ReactDOM.render(<App counter = {counter} />, document.getElementById("root"));
+ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
