c6c85721352421d1af97de81389d688fc65bdecc
Noah Sapse <nsapse@gmail.com>
2020-03-19 17:46:25 -0400

    added boilerplate code to begin exercise 1.6 et all (unicafe)

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-    return(
-        <div>
-            Hello, World!
-        </div>
-    )
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
