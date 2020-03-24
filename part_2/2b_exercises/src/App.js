f8e1697123b245495af1219c2db008d2e1aec7ad
Noah Sapse <nsapse@gmail.com>
2020-03-24 10:35:45 -0400

    finished exercise 2.8 (Phonebook Step3). forgot to make own branch, oops

--- part_2/2b_exercises/src/App.js
+++ part_2/2b_exercises/src/App.js
@@ -7,15 +7,18 @@ const App = () => {
   const [ newName, setNewName ] = useState(
     'a new person'
   )
+
+  const [number, setNumber] = useState('555-555-5555')
   
   const addName  = (event) => {
     event.preventDefault()
     const newObject = {
       name: newName,
+      number: number,
       date: new Date().toISOString(),
       id: persons.length + 1
     }
-    const found = persons.some(person => person.name.toLowerCase() === newObject.name.toLowerCase())  
+    const found = persons.some(person => person.name.toLocaleLowerCase() === newObject.name.toLowerCase())  
     if (found) {
       alert(`${newObject.name} already in Phonebook`)
     } 
@@ -27,24 +30,39 @@ const App = () => {
   const handleFormChange = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNumChange = (event) => {
+    setNumber(event.target.value)
+  }
+  
  
   return (
     <div>
       {/* <div>debug: {newName} </div> */}
       <h2>Phonebook</h2>
       <form  onSubmit = {addName}>
-        <div> name:
+        <div>
+          <p>
+          name:
           <input
           value = {newName} 
           onChange = {handleFormChange}
           />
+          </p>
+          <p>
+          number:
+          <input
+          value = {number} 
+          onChange = {handleNumChange}
+          />
+          </p>  
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-     {persons.map((person, i) => <p id = {i}>{person.name}</p>)}
+     {persons.map((person, i) => <p id = {i}>{person.name}: {person.number}</p>)}
     </div>
   )
 }
