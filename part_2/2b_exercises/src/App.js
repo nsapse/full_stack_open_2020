f10e0ec1e68429e5381582cbd359c936f2092df9
Noah Sapse <nsapse@gmail.com>
2020-03-24 09:12:06 -0400

    finished exercise 2.6 (phonebook step1)

--- part_2/2b_exercises/src/App.js
+++ part_2/2b_exercises/src/App.js
@@ -4,21 +4,41 @@ const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
-
+  const [ newName, setNewName ] = useState(
+    'a new person'
+  )
+  
+  const addName  = (event) => {
+    event.preventDefault()
+    const newObject = {
+      name: newName,
+      date: new Date().toISOString(),
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(newObject))
+  }
+  const handleFormChange = (event) => {
+    // console.log('event.target.value for form is:', event.target.value);
+    setNewName(event.target.value)
+  }
+ 
   return (
     <div>
+      {/* <div>debug: {newName} </div> */}
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
+      <form  onSubmit = {addName}>
+        <div> name:
+          <input
+          value = {newName} 
+          onChange = {handleFormChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+     {persons.map((person, i) => <p id = {i}>{person.name}</p>)}
     </div>
   )
 }
