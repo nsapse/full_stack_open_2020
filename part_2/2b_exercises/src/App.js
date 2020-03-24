c2baec0a6f2e59c16189525f13104643a17b3dbb
Noah Sapse <nsapse@gmail.com>
2020-03-24 10:37:55 -0400

    began exercise 2.9

--- part_2/2b_exercises/src/App.js
+++ part_2/2b_exercises/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState(
     'a new person'
   )
