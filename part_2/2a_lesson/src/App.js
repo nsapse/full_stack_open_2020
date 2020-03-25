6516bb1e8b8ce85d3bceb82f2164b3c663605661
Noah Sapse <nsapse@gmail.com>
2020-03-25 14:42:16 -0400

    completed lesson 2c

--- part_2/2a_lesson/src/App.js
+++ part_2/2a_lesson/src/App.js
@@ -1,12 +1,28 @@
 // import React from 'react'
 import Note from './components/Note'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfulled');
+        setNotes(response.data)
+      })
+    
+  }
+  
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes');
+  
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
