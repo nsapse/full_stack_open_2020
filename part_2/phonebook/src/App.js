661a5d8ba1511444aa73a05f4708e1040670a6ce
Noah Sapse <nsapse@gmail.com>
2020-03-25 15:17:04 -0400

    commiting changes from 2.11

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -13,11 +13,6 @@ const App = () => {
   // Does not contain ID attribute although those will likely be added 
   
   const [persons, setPersons] = useState([])
-  //   { name: 'Arto Hellas', number: '040-123456' },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
-  //   { name: 'Dan Abramov', number: '12-43-234345' },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  // ])
   
   const hook = () => {
     axios
