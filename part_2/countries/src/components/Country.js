cde17a8278cc35286dd095c704daac1fe7c2b116
Noah Sapse <nsapse@gmail.com>
2020-03-26 11:35:26 -0400

    Finished Exercise 2.13 (buttons for each country display)

--- part_2/countries/src/components/Country.js
+++ part_2/countries/src/components/Country.js
@@ -1,24 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Country = ({country, i}) => {
-    
-         return (
-            <div id = {i}>
-                <h1>{country.name}</h1>
-                <p>Capital: {country.capital}</p>
-                <p>Population: {country.population}</p>
-                <h4>Languages</h4>
-                <ul>
-                {country.languages.map((language, i) => {
-                    return(
-                        <li id = {i}>{language.name}</li>
-                    )
-                })}
-                </ul>
-                <img src={country.flag} alt=""/>
-            </div>
+      const [showFull, setShowFull] = useState(false)  
+      const buttonHandler = () => {
+        return(
+            // console.log('Button Clicked')
+          setShowFull(!showFull)
+        )
+      }
+            if (showFull === true) {
+                return(
+                <div id = {i}>
+                    <h1>{country.name}</h1>
+                    <button onClick = {buttonHandler}>Toggle Full</button>
+                    <p>Capital: {country.capital}</p>
+                    <p>Population: {country.population}</p>
+                    <h4>Languages</h4>
+                    <ul>
+                    {country.languages.map((language, i) => {
+                        return(
+                            <li id = {i}>{language.name}</li>
+                        )
+                    })}
+                    </ul>
+                    <img src={country.flag} alt=""/>
+                </div>
+                )
+            }
+            else{
+                return(
+                <div>
+                    <h1>{country.name}</h1>
+                    <button onClick = {buttonHandler}>Toggle Full</button>
+                </div>
+                )
+            }
 
-         )
             }
             
 export default Country
