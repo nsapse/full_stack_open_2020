5edf0707423e196070e9416dfdff69cf808c7ccd
Noah Sapse <nsapse@gmail.com>
2020-04-20 20:49:45 -0400

    Finished exercises 4.1 and 4.2

--- part_4/blog_list/app.js
+++ part_4/blog_list/app.js
@@ -1,39 +1,27 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
+  
+const mongoUrl = config.MONGODB_URI 
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { 
+        logger.info('connected to MongoDB')
+     })
+    .catch((error) => { 
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+    
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-  })
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = app
