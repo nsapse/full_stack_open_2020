8d8650f6be582563dd0d7191d9bcce183903d32c
Noah Sapse <nsapse@gmail.com>
2020-04-22 13:42:47 -0400

    completed exercise 4.4

--- part_4/blog_list/utils/list_helper.js
+++ part_4/blog_list/utils/list_helper.js
@@ -2,7 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const total_likes = (blogs) => {
+    return blogs.reduce((likes, blog) => likes + blog.likes, 0)
+}
+
 
 module.exports = {
-    dummy
+    dummy,
+    total_likes
 }
