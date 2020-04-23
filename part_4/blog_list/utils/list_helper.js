08a1175da6804c2cd8b0e588d10f1f2cf4b62797
Noah Sapse <nsapse@gmail.com>
2020-04-22 20:53:31 -0400

    completed exercise 4.5

--- part_4/blog_list/utils/list_helper.js
+++ part_4/blog_list/utils/list_helper.js
@@ -6,8 +6,15 @@ const total_likes = (blogs) => {
     return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
+const most_favorited_blog = (blogs) => {
+    const top_blog_post = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, 1)    
+    return {title:top_blog_post.title,
+            author: top_blog_post.author,
+            likes: top_blog_post.likes};
+}
 
 module.exports = {
     dummy,
-    total_likes
+    total_likes,
+    most_favorited_blog,
 }
