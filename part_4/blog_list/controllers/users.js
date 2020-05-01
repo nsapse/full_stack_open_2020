9d6bb7f2cc5b9b6552c8d2fd73f93f6ba3faa43d
Noah Sapse <nsapse@gmail.com>
2020-05-01 10:51:41 -0400

    added users route, no testing

--- part_4/blog_list/controllers/users.js
+++ part_4/blog_list/controllers/users.js
@@ -4,20 +4,25 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
     const body = request.body
+    const password = body.password
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
-    const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash,
-    });
-
-    const savedUser = await user.save()
-
-    response.json(savedUser)
+    if (password.length < 3) {
+        return response.status(400).json({"error":"password must be greater than three characters in lenght"})     
+    }    
     
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+        const user = new User({
+            username: body.username,
+            name: body.name,
+            passwordHash,
+        });
+
+        const savedUser = await user.save()
+
+        response.json(savedUser)
+
 })
 
 usersRouter.get('/', async (request, response) => {
