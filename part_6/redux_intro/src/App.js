15bf56cf44009208007b453a9e5eccae3f9eb6f9
Noah Sapse <nsapse@gmail.com>
2020-06-06 09:07:05 -0400

    cleanup commit

--- part_6/redux_intro/src/App.js
+++ part_6/redux_intro/src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 
 
 const store = createStore(noteReducer)
+const generateId = () => {
+  Number((Math.random() * 100000).toFixed(0)) 
+}
 
 store.dispatch({
   type: 'NEW_NOTE',
@@ -25,12 +28,49 @@ store.dispatch({
   }
 })
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return{
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+  }
+}
+
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+  
   return(
    <div>
+     <form onSubmit={addNote}>
+       <input type="text" name="note"/>
+       <button type="submit">Add</button>
+     </form>
      <ul>
        {store.getState().map( note => 
-        <li key={note.id}>
+        <li 
+          key={note.id}
+          onClick={() => toggleImportance(note.id)} 
+          >
           {note.content} <strong>{note.important ? 'important' : ''}</strong>
         </li>
         )}
