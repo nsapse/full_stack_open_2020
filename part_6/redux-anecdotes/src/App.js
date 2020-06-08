e5b629f6949f153666c478774fe2a23d5bae4a49
Noah Sapse <nsapse@gmail.com>
2020-06-08 15:35:30 -0400

    finished exercise 6.8

--- part_6/redux-anecdotes/src/App.js
+++ part_6/redux-anecdotes/src/App.js
@@ -1,33 +1,13 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { increaseVote, createAnecdote } from './reducers/anecdoteReducer'
-import AnecdoteForm from './components/anecdoteForm'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const sortedAnecdotes = anecdotes.sort((a,b) => b.votes - a.votes)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(increaseVote(id))
-  }
-
-  
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList></AnecdoteList>
       <h2>create new</h2>
       <AnecdoteForm></AnecdoteForm>
     </div>
---
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increaseVote } from '../reducers/anecdoteReducer'
+
+
+  
+const Anecdote = ({ anecdote, vote }) => {
+    return (
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={ vote }>vote</button>
+          </div>
+        </div>
+    )
+}
+
+const AnecdoteList = () => {
+   const dispatch = useDispatch()
+
+   const anecdotes = useSelector(state => state)
+   const sortedAnecdotes = anecdotes.sort((a,b) => b.votes - a.votes)
+
+   return (
+    <ul>
+      {sortedAnecdotes.map(anecdote =>
+        <Anecdote
+        anecdote={anecdote}
+        vote={() => dispatch(increaseVote(anecdote.id))}
+        />
+      )}
+    </ul>
+   )
+}
+
+
+export default AnecdoteList
+            
